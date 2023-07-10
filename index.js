45b456a5d64a75b61e62e37ba246e8f6230a0217
ManuelNDiaz <manuelndiaz1997@gmail.com>
2023-07-10 14:33:41 -0300

    Primera entrega

---
+++ index.js
@@ -0,0 +1,72 @@
+class TicketManager {
+    #PrecioBaseDeGanancia=0.15
+    constructor() {
+      this.eventos = [];
+    }
+  
+    getEventos() {
+      return this.eventos;
+    }
+  
+    agregarEvento(nombre, lugar, precio, capacidad = 50, fecha = new Date()) {
+      const evento = {
+        id: this.eventos.length + 1,
+        nombre,
+        lugar,
+        precio: precio + this.#PrecioBaseDeGanancia,
+        capacidad,
+        fecha,
+        participantes: []
+      };
+  
+      this.eventos.push(evento);
+    }
+  
+    agregarUsuario(eventoId, usuarioId) {
+      const evento = this.eventos.find(evento => evento.id === eventoId);
+      if (!evento) {
+        console.log("El evento no existe.");
+        return;
+      }
+  
+      if (evento.participantes.includes(usuarioId)) {
+        console.log("El usuario ya está registrado en este evento.");
+        return;
+      }
+  
+      evento.participantes.push(usuarioId);
+    }
+  
+    ponerEventoEnGira(eventoId, nuevaLocalidad, nuevaFecha) {
+      const eventoExistente = this.eventos.find(evento => evento.id === eventoId);
+      if (!eventoExistente) {
+        console.log("El evento no existe.");
+        return;
+      }
+  
+      const eventoEnGira = {
+        ...eventoExistente,
+        id: this.eventos.length + 1,
+        lugar: nuevaLocalidad,
+        fecha: nuevaFecha,
+        participantes: []
+      };
+  
+      this.eventos.push(eventoEnGira);
+    }
+  }
+
+  const ticketManager = new TicketManager();
+  
+  ticketManager.agregarEvento("Concierto", "Cancha", 100, 2000, new Date());
+  ticketManager.agregarEvento("Película", "Cine", 5, 50);
+  
+  console.log(ticketManager.getEventos());
+  
+  ticketManager.agregarUsuario(1, "usuario1");
+  ticketManager.agregarUsuario(1, "usuario2");
+  
+  ticketManager.ponerEventoEnGira(2, "Festival", new Date());
+  
+  console.log(ticketManager.getEventos());
+  
